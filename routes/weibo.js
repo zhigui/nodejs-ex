3ab5a938883f854ab8f11ac14e231acca7aecc23
chan <czhigui@gmail.com>
2018-05-03 20:37:48 +0800

    hello world

---
+++ routes/weibo.js
@@ -0,0 +1,85 @@
+var fetch = require('node-fetch');
+var cheerio = require('cheerio');
+var url = require('url');
+
+function getTime(html) {
+  var math;
+  var date = new Date();
+  if (math = /(\d+)分钟前/.exec(html)) {
+    date.setMinutes(date.getMinutes() - math[1]);
+    return date.toUTCString();
+  } else if (math = /今天 (\d+):(\d+)/.exec(html)) {
+    date = new Date(date.getFullYear(), date.getMonth(), date.getDate(), math[1], math[2]);
+    return date.toUTCString();
+  } else if (math = /(\d+)月(\d+)日 (\d+):(\d+)/.exec(html)) {
+    date = new Date(date.getFullYear(), math[1] - 1, parseInt(math[2]), math[3], math[4]);
+    return date.toUTCString();
+  }
+  return html;
+}
+
+module.exports = function(req, res) {
+  res.header('Content-Type', 'application/xml; charset=utf-8');
+
+  var ip = req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+
+  var query = url.parse(req.url, true).query;
+  var debug = query.debug;
+
+  var uid = req.params.uid;
+
+  fetch(`http://service.weibo.com/widget/widget_blog.php?uid=${uid}`).then(
+    response => response.text()
+  ).then((data) => {
+    var $ = cheerio.load(data, {
+      decodeEntities: false
+    });
+    var wbs = [];
+    var items = $('.wgtCell');
+    var wb, item, titleEle;
+    items.map(function(index, ele) {
+      wb = {};
+      item = $(this);
+      titleEle = item.find('.wgtCell_txt');
+      wb.title = titleEle.text().replace(/^\s+|\s+$/g, '');
+      if (wb.title.length > 24) {
+        wb.title = wb.title.slice(0, 24) + '...';
+      }
+      wb.description = titleEle.text().replace(/^\s+|\s+$/g, '').replace(/thumbnail/, 'large');
+      wb.pubDate = getTime(item.find('.link_d').html());
+      wb.link = item.find('.wgtCell_tm a').attr('href');
+      wbs.push(wb);
+    });
+    var name = $('.userNm').text();
+
+    var rss =
+      `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+<channel>
+<title>${name}的微博</title>
+<link>http://weibo.com/${uid}/</link>
+<description>${name}的微博RSS</description>
+<language>zh-cn</language>
+<lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+<ttl>300</ttl>`
+    for (var i = 0; i < wbs.length; i++) {
+      rss += `
+<item>
+    <title><![CDATA[${wbs[i].title}]]></title>
+    <description><![CDATA[${wbs[i].description}]]></description>
+    <pubDate>${wbs[i].pubDate}</pubDate>
+    <guid>${wbs[i].link}</guid>
+    <link>${wbs[i].link}</link>
+</item>`
+    }
+    rss += `
+</channel>
+</rss>`
+    res.send(rss);
+  }).catch(
+    e => console.error("Error: getting widget", e)
+  );
+};
--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+    app     = express();
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -32,75 +26,12 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
 
   }
 }
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
 
 app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+  res.send('Hello World!');
 });
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+app.get('/rss/:uid', require('./routes/weibo'));
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
